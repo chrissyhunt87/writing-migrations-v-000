e2a3333d19f1e457e58471cafb254e34f261634c
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-06 00:12:55 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,8 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration
 
-  def change_column
-    add_column(:students, :grade, :integer)
-    add_column(:students, :birthdate, :string)
+  def change
+    change_column(:students, :birthdate, :string)
   end
 
 end
